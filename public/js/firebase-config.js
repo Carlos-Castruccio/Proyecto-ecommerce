a63ef50c37353c87fdf2c2dc9857c82a0cb11b48
Carlos Castruccio <castruccio77@outlook.com>
2025-08-23 00:51:07 -0300

    actualizacion

---
+++ public/js/firebase-config.js
@@ -0,0 +1,10 @@
+// firebase-config.js - ARCHIVO DE CONFIGURACIÓN
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDUktPcM4hRkZdAkwwyUBWKzzCruCXYqYw",
+  authDomain: "motostore-accesorios-de-motos.firebaseapp.com",
+  projectId: "motostore-accesorios-de-motos",
+  storageBucket: "motostore-accesorios-de-motos.firebasestorage.app",
+  messagingSenderId: "24197467469",
+  appId: "1:24197467469:web:6ef3ebe07d5a991695fb04"
+};
